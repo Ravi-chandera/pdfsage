bbdee849fa7deb4f2f8e1c17a4a5010bc60eba49
Ravi-chandera <110690059+Ravi-chandera@users.noreply.github.com>
2023-07-07 21:26:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
-st.set_option('browser.gatherUsageStats', False)
+#st.set_option('browser.gatherUsageStats', False)
 
 def get_pdf_text(pdf_docs):
     text = ""
